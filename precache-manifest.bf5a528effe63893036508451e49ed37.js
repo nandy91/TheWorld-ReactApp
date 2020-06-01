b9b7eaa783632db74965b374bdb194dee9f92446
nandy91 <nandynew@gmail.com>
2020-05-31 22:08:18 -0400

    Updates

---
+++ precache-manifest.bf5a528effe63893036508451e49ed37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8040732ea6c3e98666c89daad6da4e",
+    "url": "/TheWorld-ReactApp/index.html"
+  },
+  {
+    "revision": "ba80e83648b626bed484",
+    "url": "/TheWorld-ReactApp/static/css/main.b6307d72.chunk.css"
+  },
+  {
+    "revision": "4e2b014d8e5a16259251",
+    "url": "/TheWorld-ReactApp/static/js/2.a2c05458.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TheWorld-ReactApp/static/js/2.a2c05458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba80e83648b626bed484",
+    "url": "/TheWorld-ReactApp/static/js/main.10d51f34.chunk.js"
+  },
+  {
+    "revision": "cc88d00d94fa7ab5b47a",
+    "url": "/TheWorld-ReactApp/static/js/runtime-main.4fbff4b0.js"
+  },
+  {
+    "revision": "6adaa174720f073005e652df43c7598c",
+    "url": "/TheWorld-ReactApp/static/media/world.6adaa174.jpg"
+  }
+]);
